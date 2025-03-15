c4c3fb0643b46f6f9b9a122b146e82180c2900c3
mysticclub <seishironagi.rukasproyect@gmail.com>
2025-03-15 12:01:35 -0500

    Actualizar descargas-hentaidl.js

--- plugins/descargas-hentaidl.js
+++ plugins/descargas-hentaidl.js
@@ -35,7 +35,7 @@ let handler = async (m, { conn, text, args, setting }) => {
           externalAdReply: {
             title: videoInfo.title,
             body: "Descargar video",
-            thumbnail: thumbnailUrl: icono,
+            thumbnail: icono,
             sourceUrl: text,
             mediaType: 1,
             renderLargerThumbnail: true
@@ -64,7 +64,7 @@ let handler = async (m, { conn, text, args, setting }) => {
           externalAdReply: {
             title: "Resultados de búsqueda",
             body: "Veohentai",
-            thumbnail: thumbnailUrl: icono,
+            thumbnail: icono,
             sourceUrl: redes,
             mediaType: 1,
             renderLargerThumbnail: true
